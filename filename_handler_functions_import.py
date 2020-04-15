0925b03a847c613b90f1481e17b9886c299caba0
Mikhail <48692715+Mikhail-Gl96@users.noreply.github.com>
2020-04-15 20:47:47 +0300

    speech to text done

--- filename_handler_functions_import.py
+++ filename_handler_functions_import.py
@@ -1,4 +1,5 @@
 # This is auto generated code
+from handlers.speech_to_text.stt import speech_to_text
 from handlers.support.help_func import help_msg
 from handlers.text_to_speech.tts import say_text_google
 from handlers.text_to_speech.tts import say_text_google
--- handlers/speech_to_text/stt.py
+++ handlers/speech_to_text/stt.py
@@ -1,68 +1,50 @@
-# from main import dp, bot
-# from aiogram.types import Message
-# import requests
-# import random
-#
-#
-# api_link_stt = 'https://speech.googleapis.com/v1p1beta1/speech:recognize'
-#
-# Request_body = {
-#   "audio": {
-#     "content": f"/* Your audio */"
-#   },
-#   "config": {
-#     "enableAutomaticPunctuation": True,
-#     "encoding": "LINEAR16",
-#     "languageCode": "ru-RU",
-#     "model": "default"
-#   }
-# }
-#
-#
-# class yTTS(object):
-#     base_url = "https://stt.api.cloud.yandex.net/speech/v1/stt:recognize"
-#
-#     def __init__(self, lang='ru_RU', **kwargs):
-#         # Инициализируем данные для запроса
-#         self.params = {
-#             "lang": self.get_lang_name(lang),
-#             "topic": 'general',
-#             "profanityFilter": False,
-#             "format": 'ogg',
-#         }
-#         self.params.update(kwargs)
-#
-#     def get_text(self):
-#         resp = requests.get(self.base_url, params=self.params, stream=True)
-#         resp.raise_for_status()
-#         print(resp.content)
-#     @staticmethod
-#     def get_lang_name(lang):
-#         # Преобразует коды стран в понятный формат для yandex speech cloud
-#         # Яндекс поддерживает только 4 языка: RU, UK, EN, TR
-#         languages = {
-#             'en': 'en_US',
-#             'ru': 'ru_RU',
-#             'tr': 'tr_TR',
-#         }
-#         if lang in languages:
-#             return languages[lang]
-#         else:
-#             return languages['ru']
-#
-#
-# # command
-# # Распознования речи в текст
-# @dp.message_handler(commands=['stt'])
-# async def speech_to_text(message: Message):
-#     reply_msg_id = message.reply_to_message.message_id
-#     voice_msg_id = message.reply_to_message.voice.file_id
-#     data_bytes = await dp.bot.get_file(voice_msg_id)
-#     print(message)
-#
-#
-#
-#     await message.answer(f'{message.reply_to_message}')
-#
-#
-#
+from main import dp
+from aiogram.types import Message
+from config import _google_cloud_key_json_name
+import os
+import io
+from google.cloud import speech_v1
+from google.cloud.speech_v1 import enums
+
+
+# command
+# Распознования речи в текст
+@dp.message_handler(commands=['stt'])
+async def speech_to_text(message: Message):
+    reply_msg_id = message.reply_to_message.message_id
+    voice_msg_id = message.reply_to_message.voice.file_id
+    data_bytes = await dp.bot.get_file(voice_msg_id)
+    await data_bytes.download()
+
+    os.environ["GOOGLE_APPLICATION_CREDENTIALS"] = _google_cloud_key_json_name
+    client = speech_v1.SpeechClient()
+    # The language of the supplied audio
+    language_code = "ru-RU"
+    # Sample rate in Hertz of the audio data sent
+    sample_rate_hertz = 48000
+    # https://googleapis.dev/nodejs/speech/latest/google.cloud.speech.v1p1beta1.html
+    encoding = enums.RecognitionConfig.AudioEncoding.OGG_OPUS
+    config = {
+        "language_code": language_code,
+        "sample_rate_hertz": sample_rate_hertz,
+        "encoding": encoding,
+    }
+    name_mp3 = data_bytes.file_path
+    with io.open(name_mp3, "rb") as f:
+        content = f.read()
+    audio = {"content": content}
+
+    response = client.recognize(config, audio)
+
+    # operation = client.long_running_recognize(config, audio)
+    # print(operation)
+    # print(u"Waiting for operation to complete...")
+    # response = operation.result()
+    # print('this is response  ')
+    answer = str()
+    for result in response.results:
+        # First alternative is the most probable result
+        alternative = result.alternatives[0]
+        answer = alternative.transcript
+        # print(u"Transcript: {}".format(alternative.transcript))
+    await message.reply(f'{answer}')
