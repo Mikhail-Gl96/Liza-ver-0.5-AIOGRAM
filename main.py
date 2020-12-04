1aa3b5a997c1ab0af5f061db7ee84658485e1e71
Mikhail <48692715+Mikhail-Gl96@users.noreply.github.com>
2020-12-04 04:46:03 +0300

    start

--- main.py
+++ main.py
@@ -6,11 +6,12 @@ from config import BOT_TOKEN, _host, _port, _login_proxy, _password_proxy, _prox
 from aiogram.contrib.fsm_storage.memory import MemoryStorage
 
 
-PROXY_AUTH = aiohttp.BasicAuth(login=_login_proxy, password=_password_proxy)
+# PROXY_AUTH = aiohttp.BasicAuth(login=_login_proxy, password=_password_proxy)
 
 loop = asyncio.get_event_loop()     # Создание асинхронного потока
 # Тип парсинга - теги как в html
-bot = Bot(token=BOT_TOKEN, parse_mode='HTML', proxy=f'{_proxy_type}://{_host}:{_port}', proxy_auth=PROXY_AUTH)
+# bot = Bot(token=BOT_TOKEN, parse_mode='HTML', proxy=f'{_proxy_type}://{_host}:{_port}', proxy_auth=PROXY_AUTH)
+bot = Bot(token=BOT_TOKEN, parse_mode='HTML')
 dp = Dispatcher(bot=bot, loop=loop, storage=MemoryStorage())  # Доставщик сообщений
 
 if __name__ == '__main__':
